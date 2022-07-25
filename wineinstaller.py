310c4309674e014f9b342f71fe66daf52ea64af6
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-07-24 19:06:46 -0500

    Update wineinstaller.py

--- wineinstaller.py
+++ wineinstaller.py
@@ -1,7 +1,7 @@
 import os
 os.system("clear")
 print("Wine Installer")
-print("A tool for installing Wine on any Debian system from Buster to Bookworm (Works for Bullseye and PIOS too!)")
+print("A tool for installing Wine on any PIOS System running PIOS Bullseye")
 print("Press Enter To Install")
 input("")
 os.system("clear")
@@ -14,17 +14,7 @@ os.system("cat LICENSE")
 print("Click enter to aggree")
 input("")
 os.system("sudo dpkg --add-architecture i386 && sudo wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key")
-osver = ""
-while not str(osver) == "1" or str(osver) == "2" or str(osver) == "3":
-  os.system("clear")
-  print("What version of debian do you have?")
-  print("1: Buster")
-  print("2: Bullseye")
-  print("3: Bookworm")
-  osver = input("Item #: ")
-if str(osver) == "1:":
-  os.system("sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/buster/winehq-buster.sources && sudo apt update && sudo apt install --install-recommends winehq-stable")
-if str(osver) == "2":
-  os.system("sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources && sudo apt update && sudo apt install --install-recommends winehq-stable")
-if str(osver) == "3":
-  os.system("sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources && sudo apt update && sudo apt install --install-recommends winehq-stable")
+os.system("sudo wget -nc -P /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources && sudo apt update && sudo apt install --install-recommends winehq-stable")
+print("Wine has been installed! Restarting in 10 seconds...")
+time.sleep(10)
+os.system("shutdown -r now")
